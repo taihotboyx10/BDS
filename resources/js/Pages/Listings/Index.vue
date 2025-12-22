ad7b78a138ecd37fa4390287886df78a0c01687e
taind41 <taind4@rikkeisoft.com>
2025-12-22 22:54:00 +0900

    add monthly payment calculator

--- resources/js/Pages/Listings/Index.vue
+++ resources/js/Pages/Listings/Index.vue
@@ -1,32 +1,12 @@
 <template>
     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <Box v-for="listing in listings" :key="listing.id">
-            <div>
-                <Link :href="route('listings.show', listing.id)">
-                    <ListingPrice :price="listing.price" class="font-bold text-2xl"/>
-                    <ListingInfo :listing="listing" class="font-bold text-lg"/>
-                    <ListingAddress :listing="listing" class="text-gray-700 dark:text-gray-200"/>
-                </Link>
-            </div>
-    
-            <div>
-                <Link :href="route('listings.edit', listing.id)">Edit</Link>
-            </div>
-    
-            <div>
-                <Link :href="route('listings.destroy', listing.id)" method="delete" as="button">Delete</Link>
-            </div>
-        </Box>
+        <ListingMain v-for="listing in listings" :key="listing.id" :listing="listing">
+        </ListingMain>
     </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import ListingAddress from '../../Components/ListingAddress.vue';
-import { route } from 'ziggy-js';
-import Box from '../../Components/UI/Box.vue';
-import ListingInfo from '../../Components/ListingInfo.vue';
-import ListingPrice from '../../Components/ListingPrice.vue';
+import ListingMain from './Components/ListingMain.vue';
 
 defineProps({
     listings: Array,
